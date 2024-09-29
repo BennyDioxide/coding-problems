a102645b19430cd60fc06fbadc85bebfa0808769
BennyDioxide <bennystyang@proton.me>
2024-09-29 20:10:38 +0800

    op(C++): Remove redundent i in TIOJ 1003

--- TIOJ/1003-1004/1003.cpp
+++ TIOJ/1003-1004/1003.cpp
@@ -6,9 +6,8 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     uint16_t n, m = 1;
-    cin >> n;
-    for (uint8_t i = 1; i <= n; i++) {
-        m += i;
+    for (cin >> n; n > 0; n--) {
+        m += n;
     }
     cout << m << '\n';
     return 0;
