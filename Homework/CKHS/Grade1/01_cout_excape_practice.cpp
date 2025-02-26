5507c17b0dffa17fd9611bd84acbe89ad4c29130
BennyDioxide <bennystyang@proton.me>
2025-02-26 16:17:20 +0800

    Add CKHS homework G1/01

---
+++ Homework/CKHS/Grade1/01_cout_excape_practice.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hi, there!\n"
+            "\"Hi, there!\"\n"
+            "\\Hi, there!\n"
+            "\\\"Hi, there!\"\n"
+            "\n"
+            "\"In C/C++, \"A\" is different from 'A' \"\n"
+            "\"Comment line use // rather than \\\\\"\n"
+            "\"Double quote \" needs to be \"\n"
+            "\\/\\/\\/\n"
+            "%%";
+
+    return 0;
+}
